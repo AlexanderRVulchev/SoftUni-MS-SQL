00909c4d183e59eb1a3b440f4b8fa09dc3681802
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-01-08 12:57:09 +0200

    Add files via upload

---
+++ 03. Table Relations/04. Self-Referencing.sql
@@ -0,0 +1,15 @@
+CREATE TABLE [Teachers]
+(	
+	[TeacherID] INT IDENTITY(101, 1) PRIMARY KEY,
+	[Name] VARCHAR(50) NOT NULL,
+	[ManagerID] INT
+		FOREIGN KEY REFERENCES [Teachers](TeacherID)
+)
+
+INSERT INTO [Teachers] VALUES
+	('John', NULL),
+	('Maya', 106),
+	('Silvia', 106),
+	('Ted', 105),
+	('Mark', 101),
+	('Greta', 101)
