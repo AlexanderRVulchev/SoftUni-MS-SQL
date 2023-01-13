45e781c70527e25850dfa491703e7ae3ece89a02
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-01-13 08:37:21 +0200

    Add files via upload

---
+++ 05. Subqueries and Joins/02. Addresses with Towns.sql
@@ -0,0 +1,11 @@
+SELECT TOP(50) 
+	e.[FirstName], 
+	e.[LastName],
+	t.[Name],
+	a.[AddressText]
+FROM [Employees] AS [e]
+JOIN [Addresses] AS [a]
+	ON e.[AddressID] = a.[AddressID]
+JOIN [Towns] AS [t]
+	ON a.[TownID] = t.[TownID]
+ORDER BY e.[FirstName], e.[LastName]
