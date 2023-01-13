899a94d48a2744182fbaa2e2046f42584e78b4a8
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-01-13 08:38:10 +0200

    Add files via upload

---
+++ 05. Subqueries and Joins/06. Employees Hired After.sql
@@ -0,0 +1,12 @@
+SELECT
+	e.[FirstName],
+	e.[LastName],
+	e.[HireDate],
+	d.[Name] AS [DeptName]
+FROM [Employees] AS [e]
+JOIN [Departments] AS [d]
+	ON e.[DepartmentID] = d.[DepartmentID]
+WHERE 
+	e.[HireDate] > '1999-01-01' AND
+	d.[Name] IN ('Sales', 'Finance')
+ORDER BY e.[HireDate]
