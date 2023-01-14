1f60fd53ddf4c5845e333d55430e065a5fdca4cf
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-01-14 10:01:31 +0200

    Add files via upload

---
+++ 05. Subqueries and Joins/11. Min Average Salary.sql
@@ -0,0 +1,10 @@
+SELECT
+	MIN(a.AverageSalary) AS MinAverageSalary
+FROM
+(
+	SELECT 
+		e.[DepartmentID],
+		AVG(e.[Salary]) AS [AverageSalary]
+	FROM [Employees] AS [e]
+	GROUP BY e.[DepartmentID]
+) AS [a]
