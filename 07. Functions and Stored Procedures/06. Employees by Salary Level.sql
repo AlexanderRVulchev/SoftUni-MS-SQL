2501975586f54ec03deb8ed286db8bfe9dd27c99
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-01-21 11:42:49 +0200

    Add files via upload

---
+++ 07. Functions and Stored Procedures/06. Employees by Salary Level.sql
@@ -0,0 +1,8 @@
+CREATE PROC usp_EmployeesBySalaryLevel
+(@levelOfSalary VARCHAR(7)) 
+AS
+	SELECT
+		[FirstName],
+		[LastName]
+	FROM [Employees]
+	WHERE dbo.ufn_GetSalaryLevel(Salary) = @levelOfSalary
